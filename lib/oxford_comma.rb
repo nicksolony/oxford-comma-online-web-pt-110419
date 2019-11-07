59dc8263dd703853c971cbfef0e6c2b736e5661d
Mykola Solonyy <nicksolony@gmail.com>
2019-11-07 03:43:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.length == 1 do
+  if array.length == 1
     array.join(",")
   elsif
-    array.length == 2 do
+    array.length == 2 
       array.join(" and ")
   else
     array[-1] = " and #{array [-1]}"
