f2c4b67acba451da07c0986a7986b40fc17cff4b
Mykola Solonyy <nicksolony@gmail.com>
2019-11-07 03:42:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
-  array.length == 2 ? array.join(" and ") : array.join(",")
+  if array.length == 1 do
+    array.join(",")
+  elsif
+    array.length == 2 do
+      array.join(" and ")
+  else
+    array[-1] = " and #{array [-1]}"
+    array.join(",")
   
 end
